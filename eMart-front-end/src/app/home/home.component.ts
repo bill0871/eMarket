c00ece1ef73c16183a34beb4b4a2c0ff0377ceff
Bill <pengbill22@gmail.com>
2020-04-26 17:02:57 +0800

    Produce home page

--- eMart-front-end/src/app/home/home.component.ts
+++ eMart-front-end/src/app/home/home.component.ts
@@ -1,33 +1,108 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  products = [
-    {
-      id: "ABC8441189035",
-      name: "Tshirt",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-    },
-    {
-      id: "DEF6510463347",
-      name: "Shoes",
-      description: "Proin ac metus in diam porttitor viverra eu sit amet ligula."
-    },
-    {
-      id: "GHI0831819467",
-      name: "Handbags",
-      description: "Duis sodales dui vitae urna varius, at ullamcorper purus tempor."
+    products = [
+        {
+            id: "ABC8441189035",
+            name: "Tshirt",
+            price: 299.99
+        },
+        {
+            id: "DEF6510463347",
+            name: "Shoes",
+            price: 299.99
+        },
+        {
+            id: "GHI0831819467",
+            name: "Handbags",
+            price: 299.99
+        },
+        {
+            id: "ABC8441189035",
+            name: "Tshirt",
+            price: 299.99
+        },
+        {
+            id: "DEF6510463347",
+            name: "Shoes",
+            price: 299.99
+        },
+        {
+            id: "GHI0831819467",
+            name: "Handbags",
+            price: 299.99
+        },
+        {
+            id: "ABC8441189035",
+            name: "Tshirt",
+            price: 299.99
+        },
+        {
+            id: "DEF6510463347",
+            name: "Shoes",
+            price: 299.99
+        },
+        {
+            id: "GHI0831819467",
+            name: "Handbags",
+            price: 299.99
+        },
+        {
+            id: "ABC8441189035",
+            name: "Tshirt",
+            price: 299.99
+        },
+        {
+            id: "DEF6510463347",
+            name: "Shoes",
+            price: 299.99
+        },
+        {
+            id: "GHI0831819467",
+            name: "Handbags",
+            price: 299.99
+        },
+        {
+            id: "ABC8441189035",
+            name: "Tshirt",
+            price: 299.99
+        },
+        {
+            id: "DEF6510463347",
+            name: "Shoes",
+            price: 299.99
+        },
+        {
+            id: "GHI0831819467",
+            name: "Handbags",
+            price: 299.99
+        },
+        {
+            id: "ABC8441189035",
+            name: "Tshirt",
+            price: 299.99
+        },
+        {
+            id: "DEF6510463347",
+            name: "Shoes",
+            price: 299.99
+        },
+        {
+            id: "GHI0831819467",
+            name: "Handbags",
+            price: 299.99
+        }
+    ]
+
+    constructor() {
     }
-  ]
 
-  constructor() {
-  }
-
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
